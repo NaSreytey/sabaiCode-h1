80db84c5b352ce960e534fd962f124fc9af6ef13
NaSreytey <nasreytey19@gmail.com>
2024-07-04 11:17:49 +0700

    done

---
+++ homework.js
@@ -0,0 +1,109 @@
+// 1. print 1 to 10
+//     for(let i=0;i<=10;i++){
+//         console.log(i);
+//     }
+
+//2. print 10 to 1
+//     for(let i=10;i>=1;i--){
+//         console.log(i);
+//     }
+
+// 3. convert munber to letter
+// let score=50;
+// if(score>=90){
+//     console.log("grade A");
+// }else if(score>=80){
+//     console.log("grade B");
+// }else if(score>=70){
+//     console.log("grade C");
+// }else if(score>=60){
+//     console.log("grade D");
+// }else if(score>=50){
+//     console.log("grade E");
+// }else{
+//     console.log("grade F");
+// }
+
+//4. 
+
+// let employees = ["Lara", "Evee", "Simi"]
+// let salary = [1000, 2000, 120.90]
+//       for(let i=0;i<salary.length;i++){
+//         let employeesName=employees[i];
+//         let salaryNum=salary[i];
+//         let connect= `${employeesName}:${salaryNum}`;
+//         console.log(connect);
+//       }
+
+      //.5
+    //   for(let i=1;i<=10;i++){
+    //     if(i%2 !=0){
+    //         console.log(i);
+    //     }
+    //   }
+
+    //6.
+
+    // let evenCount = 0;
+    // let oddCount = 0;
+    
+    // for(let i=1;i<=10;i++){
+    //     if(i%2 ==0){
+    //         evenCount++;
+    //     }else{
+    //         oddCount++;
+    //     } 
+    // }
+    // console.log("evenNumber = "+evenCount);
+    // console.log("oddNumber = "+oddCount);
+
+    //7.
+
+    // for(let i=1;i<=100;i++){
+    //     if(i%3==0 && i%5==0 ){
+    //      console.log("FizzBuzz")
+    //     }else if(i%3==0){
+    //         console.log("Fizz");
+    //     }else if(i%5==0){
+    //         console.log("Buzz");
+    //     }else{
+    //         console.log(i)
+    //     }
+    // }
+
+    //8.
+
+    // const arrays=[12,43,55,67,87,67]
+    // let sum =0;
+    // for(let i=0;i<arrays.length;i++){
+    //     sum += arrays[i];
+    // }
+    // console.log(sum)
+
+    //9.
+    // let primeNum=true;
+    // for(let i=1;i<=100;i++){
+    //   primeNum=true;
+    //   for(let j=2;j<i;j++){
+    //     if(j!=i && i%j==0){
+    //       primeNum=false
+    //     }
+    //   }
+    //   if(primeNum&&i!=1){
+    //     console.log(i) 
+
+    //   }
+    // }
+
+
+    //10.
+    // let numbers = [3, 17, 24, 86, 58];
+    // let max = numbers[0];
+    
+    // for (let i = 1; i < numbers.length; i++) {
+    //     if (numbers[i] > max) {
+    //         max = numbers[i];
+    //     }
+    // } 
+    
+    // console.log(max);
